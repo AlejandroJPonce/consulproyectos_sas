44fb7886d75eb6505e545caf174608671df2a48d
Alejandro Ponce <alejandroponce@Alejandros-MacBook-Pro.local>
2025-09-07 21:14:37 -0500

    fix responsive

--- src/components/cards/ButtonCard.vue
+++ src/components/cards/ButtonCard.vue
@@ -3,17 +3,17 @@ const props = defineProps(["icon", "title", "size"]);
 </script>
 <template>
   <div
-    class="w-[280px] h-[241px] bg-white rounded-2xl flex flex-col items-center justify-center"
+    class="w-auto sm:w-[280px] h-auto sm:h-[241px] bg-white rounded-2xl flex flex-col items-center justify-center p-3 pb-7"
   >
-    <div class="h-[140px] md:h-[160px] lg:h-[160px] flex items-center justify-center">
+    <div class=" h-20 md:h-[160px] sm:h-[160px] flex items-center justify-center">
       <img
-        class="w-[110px] md:w-[130px] lg:w-[130px]"
+        class="w-[60px] sm:w-[130px]"
         :src="props.icon"
         alt=""
       />
     </div>
-    <div class="w-full text-center">
-      <span class="text-black font-semibold text-center text-lg">{{
+    <div class="w-[120px] sm:w-full text-center">
+      <span class="text-black font-semibold text-center text-sm sm:text-lg">{{
         props.title
       }}</span>
     </div>
